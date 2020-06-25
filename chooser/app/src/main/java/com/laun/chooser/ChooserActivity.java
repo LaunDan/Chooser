e8b5c4e2bd8aa9336fb9c3f8c9bd53e919a29c2b
LaunDan <launadaniel2@gmail.com>
2020-06-25 16:20:08 +0200

    created new layout for chooser activity

--- chooser/app/src/main/java/com/laun/chooser/ChooserActivity.java
+++ chooser/app/src/main/java/com/laun/chooser/ChooserActivity.java
@@ -11,7 +11,7 @@ public class ChooserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chooser);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
     }
 }
