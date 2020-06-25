31f3318d018f26aa4eb8f303d1b9c3196bc7bc01
LaunDan <launadaniel2@gmail.com>
2020-06-25 16:16:21 +0200

    created new activity class chooser

---
+++ chooser/app/src/main/java/com/laun/chooser/ChooserActivity.java
@@ -0,0 +1,17 @@
+package com.laun.chooser;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+    }
+}
