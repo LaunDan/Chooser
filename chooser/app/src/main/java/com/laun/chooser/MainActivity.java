f6b8e10377c4e853d3c54ce9332742cd07437f97
LaunDan <launadaniel2@gmail.com>
2020-06-26 20:11:59 +0200

    change editText
    put intend

--- chooser/app/src/main/java/com/laun/chooser/MainActivity.java
+++ chooser/app/src/main/java/com/laun/chooser/MainActivity.java
@@ -7,9 +7,12 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private String choices;
+    private EditText choicesET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,12 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        choicesET = findViewById(R.id.choicesET);
     }
 
     public void choose(View view) {
+        choices = choicesET.getText().toString();
         Intent chooser = new Intent(this, ChooserActivity.class);
         chooser.putExtra("CHOICES", choices);
         startActivity(chooser);
