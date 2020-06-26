1bac1755c68e3cb9576653f408a088a7ed703cd5
LaunDan <launadaniel2@gmail.com>
2020-06-26 20:16:27 +0200

    get intend

--- chooser/app/src/main/java/com/laun/chooser/ChooserActivity.java
+++ chooser/app/src/main/java/com/laun/chooser/ChooserActivity.java
@@ -7,11 +7,16 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChooserActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_chooser);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        String choices = getIntent().getStringExtra("CHOICES");
+        
     }
 }
