24ecfbfcee2cbb689e63349a7ffc228e873513d4
LaunDan <launadaniel2@gmail.com>
2020-06-26 18:29:41 +0200

    anabled to show new activity

--- chooser/app/src/main/java/com/laun/chooser/MainActivity.java
+++ chooser/app/src/main/java/com/laun/chooser/MainActivity.java
@@ -2,11 +2,14 @@ package com.laun.chooser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private String choices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+    }
+
+    public void choose(View view) {
+        Intent chooser = new Intent(this, ChooserActivity.class);
+        chooser.putExtra("CHOICES", choices);
+        startActivity(chooser);
     }
 }
